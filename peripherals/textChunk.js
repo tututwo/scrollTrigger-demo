d8acbc7e1ba5651457abc6511a674f14e3e69a7c
Gordon Tu <gordontu@Gordons-Mac-Studio.local>
2023-07-30 12:55:20 -0400

    text added

--- peripherals/textChunk.js
+++ peripherals/textChunk.js
@@ -22,6 +22,42 @@ function textChunk() {
       index = parseInt(Math.round(sc.progress * 10));
 
       d3.select("#bubble-map-text-chunks h4").text(yearList[index]);
+
+      if (yearList[index] === 2008) {
+        document
+          .querySelector(".bubble-map-blurb-0")
+          .classList.add("is-visible");
+        document
+          .querySelector(".bubble-map-blurb-1")
+          .classList.remove("is-visible");
+      } else if (yearList[index] === 2011) {
+        document
+          .querySelector(".bubble-map-blurb-0")
+          .classList.remove("is-visible");
+        document
+          .querySelector(".bubble-map-blurb-1")
+          .classList.add("is-visible");
+        document
+          .querySelector(".bubble-map-blurb-2")
+          .classList.remove("is-visible");
+      } else if (yearList[index] === 2016) {
+        document
+          .querySelector(".bubble-map-blurb-1")
+          .classList.remove("is-visible");
+        document
+          .querySelector(".bubble-map-blurb-2")
+          .classList.add("is-visible");
+        document
+          .querySelector(".bubble-map-blurb-3")
+          .classList.remove("is-visible");
+      } else if (yearList[index] > 2016) {
+        document
+          .querySelector(".bubble-map-blurb-2")
+          .classList.remove("is-visible");
+        document
+          .querySelector(".bubble-map-blurb-3")
+          .classList.add("is-visible");
+      }
     },
   });
 }
